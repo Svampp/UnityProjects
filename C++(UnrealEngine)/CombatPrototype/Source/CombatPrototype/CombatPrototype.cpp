e213a51017e7cfb65004f6ee091436539458bada
Anna <anna.pennie04@gmail.com>
2025-04-28 19:34:01 +0300

    Remove project for now

--- C++(UnrealEngine)/CombatPrototype/Source/CombatPrototype/CombatPrototype.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CombatPrototype.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CombatPrototype, "CombatPrototype" );
- 
